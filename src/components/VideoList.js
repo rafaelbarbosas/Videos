542a9a88af85c273a82ecff42ba994145566fb7c
Rafael Barbosa <rafael.1940.b@gmail.com>
2020-02-01 16:09:20 -0300

    Adiciona descrição do vídeo, ao clicar num dos itens

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos,onVideoSelect }) => {
     const renderedList = videos.map((video) => (
-        <VideoItem video={video} />
+        <VideoItem
+            onVideoSelect={onVideoSelect}
+            video={video}
+        />
     ));
 
 return <div className="ui relaxed divided list">{renderedList}</div>;
